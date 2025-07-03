abbe0f8fc1b16e003ee099aebf735f7ca23bff35
wardanaricho0 <wardanaricho0@gmail.com>
2025-07-03 17:46:14 +0700

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-# Permission folder Laravel
+# Set permission
 chown -R www-data:www-data storage bootstrap/cache
 chmod -R ug+rwx storage bootstrap/cache
+
+# Generate app key jika belum ada
+if [ ! -f .env ]; then
+  cp .env.example .env
+fi
+
+php artisan key:generate
+
+# Jalankan migrasi database (jika sudah ada koneksi DB)
+php artisan migrate --force
 
 # Jalankan php-fpm
 exec php-fpm
